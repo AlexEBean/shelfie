c2eb4b32ea75f18c31b58dd34050cebd43712055
Alex Bean <alexbean95@gmail.com>
2020-11-06 12:14:04 -0700

    P1 S4

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react'
 import Header from "./components/Header/Header"
 import Dashboard from './components/Dashboard/Dashboard'
 import Form from './components/Form/Form'
+import axios from "axios"
 
 import "./App.css"
 
@@ -9,17 +10,20 @@ class App extends Component {
   constructor(){
     super()
     this.state = {
-      inventory: [{
-        name: "Bacon",
-        imageURL: "",
-        price: 5
-      },
-      {
-        name: "SayWhat",
-        imageURL: "",
-        price: 8
-      }]
+      inventory: []
     }
+  }
+
+  componentDidMount() {
+    this.getInventory()
+  }
+
+  getInventory = () => {
+    axios.get("/api/inventory")
+      .then((res) => {
+        this.setState({inventory: res.data });
+      })
+      .catch((err) => console.log(err))
   }
 
   render() {
